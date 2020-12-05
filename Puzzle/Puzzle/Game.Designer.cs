b690575b4e360ef84dda6a946a6f1bff6cdacc8c
LAPTOP-D781OGGH\evges <evgesha.kaverina@mail.ru>
2020-12-05 23:13:02 +0400

    игровое поле

--- Puzzle/Puzzle/Game.Designer.cs
+++ Puzzle/Puzzle/Game.Designer.cs
@@ -28,12 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(31, 37);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(674, 380);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // Game
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Game";
             this.Text = "Game";
+            this.Load += new System.EventHandler(this.Game_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
     }
 }
--- Puzzle/Puzzle/Game.cs
+++ Puzzle/Puzzle/Game.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,44 @@ namespace Puzzle
 {
     public partial class Game : Form
     {
+        private GalleryForCreate gal;
         public Game()
         {
             InitializeComponent();
+        }
+
+        Image image;
+        PictureBox pic = null;
+
+        private void Game_Load(object sender, EventArgs e)
+        {
+
+        }
+        private Bitmap CreateBitmapImage(string pict, Image image)
+        {
+           // string img = gal.getpicture_name();
+
+            if (pic == null)
+            {
+                Bitmap bm = new Bitmap(Path.Combine(Directory.GetCurrentDirectory(), @"..\..\gallery\cat4", pict));
+                pic = new PictureBox();
+                pic.Height = groupBox1.Height;
+                pic.Width = groupBox1.Width;
+                groupBox1.Controls.Add(pic);
+            }
+            pic.Image = image;
+
+            //Image img = Image.FromFile(@"..\..\gallery\about.jpg");
+
+
+            Bitmap objBmImage = new Bitmap(groupBox1.Width, groupBox1.Height);
+            Graphics objGraphics = Graphics.FromImage(objBmImage);
+            objGraphics.Clear(Color.White);
+            objGraphics.DrawImage(image, new Rectangle(0, 0, groupBox1.Width, groupBox1.Height));
+            objGraphics.Flush();
+
+            return objBmImage;
+            
         }
     }
 }
