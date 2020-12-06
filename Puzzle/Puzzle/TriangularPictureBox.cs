24dba78e20c24b9326220f6288bbc32704e9b11f
Alsu <alsu.ispuhaleeva@gmail.com>
2020-12-06 16:09:50 +0400

    add TriangularPixtureBox

---
+++ Puzzle/Puzzle/TriangularPictureBox.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace Puzzle
+{
+    class TriangularPictureBox : MyPictureBox
+    {
+        private bool left;
+        public bool LeftGrag
+        {
+            set { left = value; }
+        }
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            using (var p = new GraphicsPath())
+            {
+                if (left)
+                {
+                    p.AddPolygon(new Point[] {
+                new Point(0, 0),
+                new Point(0, Height),
+                new Point(Width, 0) });
+                }
+                else
+                {
+                    p.AddPolygon(new Point[] {
+                new Point(Width, Height),
+                new Point(0, Height),
+                new Point(Width, 0) });
+                }
+                this.Region = new Region(p);
+                base.OnPaint(pe);
+            }
+        }
+    }
+}
